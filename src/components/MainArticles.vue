3ab0ac37e8ac6097992f909fb90d564d6cacde85
pierfrancescoscar <scarinci401@gmail.com>
2021-12-09 18:25:28 +0100

    ADD: main articles component in home

--- src/components/MainArticles.vue
+++ src/components/MainArticles.vue
@@ -6,9 +6,20 @@
                   <nav>
                       <ul class="d-flex flex-wrap ps-0">
                           <li class="list-unstyled pe-4" v-for="(post, index) in articles" :key="`article-${index}`">
+                              <div>
                                   <img class="img-fluid" :src="require(`@/assets/${post.image}.jpg`)" :alt="post.text">
-                                  <a :href="post.url">{{post.text}}</a>
-                                  <span>test</span>
+
+                                    <div class="post-content w-100">
+                                        <div class="category-tag pb-3">
+                                            <span class="text-white text-uppercase p-2 rounded">
+                                                {{post.category}}
+                                            </span>
+                                        </div>
+                                        <div class="post-title w-100">
+                                            <a class="text-decoration-none text-white" :href="post.url">{{post.text}}</a>
+                                        </div>
+                                    </div>
+                              </div>
                           </li>
                       </ul>
                   </nav>
@@ -37,8 +48,30 @@ section {
 
     li {
         width: calc(100% / 3);
+        position: relative;
     
     }
-    
 
+    span {
+        font-weight: $medium-fw;
+        font-size: $minor-fs;
+        background-color: $background-4;
+    }
+
+    a {
+        font-size: $medium-fs;
+        font-weight: $extrastrong-fw;
+    }
+
+    li:nth-child(1), li:nth-child(2), li:nth-child(3) {
+        margin-bottom: 1.5rem;
+    }
+
+    .post-content { 
+        position: absolute;
+        bottom: 20px;
+        left: 20px;
+    }
+        
+    
 </style>
