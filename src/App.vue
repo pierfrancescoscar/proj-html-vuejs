5c4425078a5c0cc0fe8d4ce2160e71561a2f5b06
pierfrancescoscar <scarinci401@gmail.com>
2021-12-09 13:24:47 +0100

    ADD: header logo component

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,22 @@
     <header>
     <!-- Blue Top Header Navigation -->
       <TopHeader />
+    <!-- Logo -->
+      <HeaderLogo />
     </header>
   </div>
 </template>
 
 <script>
 import TopHeader from '@/components/TopHeader.vue';
+import HeaderLogo from '@/components/HeaderLogo.vue';
 
 
 export default {
   name: 'App',
   components: {
     TopHeader,
+    HeaderLogo,
     
   }
 }
---
+++ src/components/HeaderLogo.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="container-fluid p-3">
+      <div class="container">
+          <div class="row">
+              <div class="col-12">
+                  <img class="py-4" src="../assets/logo-default-slim.png" alt="porto-logo">
+              </div>
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'HeaderLogo',
+
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/style/vars.scss';
+
+section {
+    background-color: $background-1;
+    border-bottom: 1px solid $border-3;
+}
+    img {
+        width: 100px;
+    }
+
+</style>
